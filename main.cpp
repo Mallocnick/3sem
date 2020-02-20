52fc04d8039b0e04d1c7cbd8b40a91b6d813cda8
Vladislav Kim <vladis.kim5@gmail.com>
2020-02-20 18:50:44 +0300

    update

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@ int main(int argc, char *argv[])
     // Каждое Qt5-приложение (за исключением консольных) должно включать следующую строчку
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowTitle("Coursework"); // устанавливаем заголовок для окна
     w.setToolTip("Coursework"); // устанавливаем всплывающую подсказку для виджета
     w.setWindowIcon(QIcon("C:/Users/vladi/Documents/3sem/steamicon.png"));
     w.show(); // выводим на экране
